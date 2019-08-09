8f43c368119aab2ddaa75bcc99ad380b75389420
Mahlon Collins <mahlon@umn.edu>
2019-08-09 13:51:35 -0500

    Add 'doc_functions.el' for easier access to help buffers.

---
+++ doc_functions.el
@@ -0,0 +1,121 @@
+;; -----
+(defun mac-man-page ()
+  "Get a manpage while editing bash code."
+  (interactive)
+  (let ((page (thing-at-point 'word t)))
+    (when
+        (= 1 (shell-command (concat "which " page)))
+      (setq page
+            (completing-read
+             "help on: "
+             'Man-completion-table nil nil nil 'Man-topic-history)))
+    (unless (not page)
+      (eshell/man page))))
+
+
+
+
+;; -----
+;; for python completion
+(defun jedi:ac-direct-matches ()
+  (mapcar
+   (lambda (x)
+     (cl-destructuring-bind (&key word doc description symbol)
+         x
+       (popup-make-item word
+                        :symbol symbol
+                        :document (unless (equal doc "") doc)
+                        :summary description)))
+   jedi:complete-reply))
+
+
+
+
+;; -----
+(defun mac-python-help-doc ()
+  "Print the result of calling 'help(symbol)' in python to a temporary
+help buffer.  If point is at a symbol, call the documentation for that
+symbol.  Otherwise, prompt for a symbol via 'read-string'.  Once the
+documentation buffer has been displayed, return point to the previous
+window and location in the current buffer."
+  (interactive)
+(let ((python-help-buffer (get-buffer-create "*Python-Help*"))
+      (python-symbol (symbol-at-point))
+      (python-string))
+
+(when (not python-symbol)
+  (setq python-symbol
+        (completing-read "Help on: " (jedi:ac-direct-matches))))
+
+  (setq python-string
+        (concat "help('"
+                (format "%s" python-symbol)
+                "')"))
+
+  (with-output-to-temp-buffer python-help-buffer
+    (prin1 (python-shell-send-string-no-output python-string) python-help-buffer))
+
+(pop-to-buffer python-help-buffer)
+(beginning-of-buffer)
+(help-mode)
+))
+
+
+
+
+;; -----
+(defun mac-master-documentation ()
+  "My function for context-aware calling of documentation for R, python, shell, or elisp.
+  Relies on various helper functions."
+  (interactive)
+  (let ((cb (current-buffer))
+        (cp (point))
+        (completions '(("ess-mode" ess-mode) ("sh-mode" sh-mode) ("python-mode" python-mode) ("emacs-lisp-mode" emacs-lisp-mode)))
+        (mm major-mode)
+        (topic (thing-at-point 'word t)))
+
+    (when (not (bufferp (get-buffer "*R*")))
+      (rstart))
+
+    (when (eq mm 'org-mode)
+      (setq mm (cadr (assoc (completing-read "mode: " completions) completions))))
+
+    (cond
+     ;; R help - use "*R*" as current buffer to get completions 
+     ((eq mm 'ess-mode)
+        (if (or
+             (not topic)
+             ;; deal with thing-at-point not handling "." containing sexps
+             ;; need to double the 'with-current-buffer' or the looking-at test runs in R
+             (or (looking-at "[[:alnum:]]+[._]")
+                 (looking-back "[[:alnum:]]+[._][[:alnum:]]+")))
+                 (with-current-buffer "*R*"
+                 (call-interactively 'ess-display-help-on-object))
+                 (with-current-buffer "*R*"
+                 (ess-display-help-on-object topic))))
+
+     ;; python help
+     ((eq mm 'python-mode)
+      (progn
+        (mac-python-help-doc)))
+
+     ;; shell help
+     ((or
+       (eq mm 'sh-mode)
+       (eq mm 'shell-mode)
+       (eq mm 'eshell-mode))
+       (mac-man-page))
+
+     ;; elisp help
+     (t
+      (if (functionp (symbol-at-point))
+          (describe-function (symbol-at-point))
+      (call-interactively 'describe-function)))
+     )
+
+    (unless
+        (eq (window-buffer) cb)
+      (progn
+        (switch-to-buffer-other-window cb)
+        (goto-char cp)))
+    ))
